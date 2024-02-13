c4fb88c093cb2ec586cffacb24368b76c1c08433
burtlau <bert.lau@outlook.com>
2024-02-13 17:32:52 -0500

    made changes to the main demo application

--- src/main/java/com/example/Homes/DemoApplication.java
+++ src/main/java/com/example/Homes/DemoApplication.java
@@ -20,56 +20,50 @@ public class DemoApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@Bean
-	CommandLineRunner runner(PropertyRepository repository)
-	{
-		return args -> {
-			Address address = new Address(
-					"Canada",
-					"Toronto",
-					"M5V 0R1");
 
-			Property house = new House(
-					PropertyType.House,
-					80.7,
-                    900000.0,
-					address,
-					2,
-					2,
-					"",
-                    "1",
-					true,
-					2
-			);
-
-			PropertyServiceImpl propertyService = new PropertyServiceImpl(repository);
-			Property addedProperty = propertyService.addProperty(house);
-			String addedPropertyId = addedProperty.getId();
-
-
-			Property house2 = new House(
-					PropertyType.House,
-					80.7,
-					1000000.0,
-					address,
-					2,
-					2,
-					"",
-					"1",
-					true,
-					2
-			);
-
-//			repository.save(house);
-//			propertyService.deleteAll();
-			propertyService.updateProperty(addedPropertyId, house2);
-			// Print out the retrieved properties
-//			System.out.println("Properties in the database:");
-//			for (Property property: properties) {
-//				System.out.println(property);
-//			}
-
-
-		};
-	}
+//
+//	@Bean
+//	CommandLineRunner runner(PropertyRepository repository)
+//	{
+//		return args -> {
+//			Address address = new Address(
+//					"Canada",
+//					"Toronto",
+//					"M5V 0R1");
+//
+//			Property house = new House(
+//					PropertyType.House,
+//					80.7,
+//                    900000.0,
+//					address,
+//					2,
+//					2,
+//					"",
+//                    "1",
+//					true,
+//					2
+//			);
+//
+//			PropertyServiceImpl propertyService = new PropertyServiceImpl(repository);
+//			Property addedProperty = propertyService.addProperty(house);
+//			String addedPropertyId = addedProperty.getId();
+//
+//
+//			Property house2 = new House(
+//					PropertyType.House,
+//					80.7,
+//					1000000.0,
+//					address,
+//					2,
+//					2,
+//					"",
+//					"1",
+//					true,
+//					2
+//			);
+//			propertyService.updateProperty(addedPropertyId, house2);
+//
+//
+//		};
+//	}
 }
